cc14bf4e0114ebb449a82fcccf0113ef3c5a4921
Dustin Swartzbaugh <dustin.swartzbaugh@gmail.com>
2022-11-14 10:26:32 -0500

    POST

--- api-server/migration.sql
+++ api-server/migration.sql
@@ -1,14 +1,15 @@
 DROP TABLE IF EXISTS check_list;
 
 CREATE TABLE check_list (
+  cat varchar(25),
   task varchar(25),
   details varchar(250),
   allotted_time_min int, 
   date_time timestamp
 );
 
-INSERT INTO check_list (task, details, allotted_time_min, date_time)
-VALUES ('vacuum', 'vacuum all rugs and carpets', 30, NULL),
-('dishes', 'load and start dishwasher', 60, NULL),
-('laundry', 'gather all clothing/bedding to be washed, wash/dry/fold', 90, NULL),
-('doctor', 'apt at providence on 5th for annual physical', NULL, '2022-11-11 13:30:00.000');
+INSERT INTO check_list (cat, task, details, allotted_time_min, date_time)
+VALUES ('cleaning', 'vacuum', 'vacuum all rugs and carpets', 30, NULL),
+('cleaning','dishes', 'load and start dishwasher', 60, NULL),
+('cleaning','laundry', 'gather all clothing/bedding to be washed, wash/dry/fold', 90, NULL),
+('appointment', 'doctor', 'apt at providence on 5th for annual physical', NULL, '2022-11-11 13:30:00.000');
--- api-server/server.js
+++ api-server/server.js
@@ -17,6 +17,47 @@ app.use(express.json());
 
 app
   .route('/api/list')
+  .get((req,res)=>{
+    client.query(`SELECT * FROM check_list`)
+    .then(result=>{
+      console.log('first', result.rows)
+      res.status(200).set('Content-Type', 'application/json').send(result.rows)
+    })
+    .catch(e => console.error(e.stack))
+  })
+
+  .post((req,res)=>{
+    let list = req.body;
+    let cat = list.cat
+    let task = list.task;
+    let details = list.details;
+    let howLong = list.allotted_time_min;
+    let when = list.date_time;
+    client.query(`INSERT INTO check_list (cat, task, details, allotted_time_min, date_time)
+    VALUES ('${cat}', '${task}', '${details}', ${howLong}, ${when}) RETURNING *`)
+    .then(result=>{
+      res.status(200).set('Content-Type', 'application/json').send(result.rows)
+    })
+    .catch(e => console.error(e.stack))
+  })
+
+app
+  .route('/api/list/:id')
+  .get((req,res)=>{
+    client.query(`SELECT * FROM check_list WHERE task = ${req.params.id}`)
+    .then(result=>{
+      res.status(200).set('Content-Type','application/json').send(result.rows)
+    })
+    .catch(e => console.error(e.stack))
+  })
+
+  .patch((req,res)=>{
+
+  })
+
+  .delete((req,res)=>{
+    client.query(`DELETE FROM check_list WHERE `)
+  })
 
 app.listen(PORT, ()=>{
   console.log(`Listening on port ${PORT}`)
